763a99d0c4328950fcb4736e1b30ff388a307cc4
Margo <margo@margonline.co.uk>
2013-10-01 22:50:23 +0100

    jquery for showing favourite links using setTimeout

--- public/js/application.js
+++ public/js/application.js
@@ -5,7 +5,19 @@ function addFavouritesHandler(){
     var newOpacity = favourited ? 0 : 1;
     $(link).data("favourited", !favourited);
     $(this).animate({opacity: newOpacity}, 1000);
+    showLinkFavouritedNotice(link);
   });
+}
+
+function showLinkFavouritedNotice(link){
+  var favourited = !!(link).data("favourited");
+  var name = $(link).find('.title').text();
+  var message = favourited ? name + "was added to favourites" : name + "was removed from favourites";
+  var flash = $("<div></div>").addClass('flash').addClass('notice').html(message);
+  $(flash).appendTo('#flash-container');
+  setTimeout(function(){
+    $(flash).fadeOut();
+  }, 3000);
 }
 
 $(function(){
